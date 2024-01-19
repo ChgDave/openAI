1b4e0c34cc26f360d5435b029554ed2a7d12b916
ChgDave <chgdave@gmail.com>
2024-01-19 14:17:08 -0600

    added console.log

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ userInterface.on("line", async (input) => {
     messages: [{ role: "user", content: input }],
     model: "gpt-3.5-turbo",
   });
+  console.log(completion.choices[0]);
   console.log(completion.choices[0].message.content);
   const response = await openai.audio.speech.create({
     model: "tts-1",
@@ -38,21 +39,3 @@ userInterface.on("line", async (input) => {
     }
   });
 });
-
-// async function createSpeech() {
-//   const response = await openai.audio.speech.create({
-//     model: "tts-1",
-//     voice: "alloy",
-//     input: "Today is a wonderful day to build something people love!",
-//   });
-//   console.log(response);
-//   const audioData = await response.arrayBuffer();
-//   fs.writeFileSync("output.mp3", Buffer.from(audioData));
-//   player.play("output.mp3", (err) => {
-//     if (err) {
-//       console.error("Error playing audio:", err);
-//     }
-//   });
-// }
-
-// createSpeech();
