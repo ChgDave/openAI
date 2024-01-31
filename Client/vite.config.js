9a36dd7053e0e8384efba5eba9df8ee5a3501753
ChgDave <chgdave@gmail.com>
2024-01-30 22:25:34 -0600

    added comments for proxy

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// why does the proxy not working??
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
